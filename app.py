f3a16927b533e568e7474d72e599ea1e24a1f5df
abinand-py <abinivess16@gmail.com>
2025-08-09 01:13:17 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.set_page_config(page_title="Happy Raksha Bandhan!", page_icon="🎁")
+
+st.markdown(
+    "<h1 style='text-align: center; color: #8e44ad;'>🎀 Happy Raksha Bandhan, Harithra! 🎀</h1>",
+    unsafe_allow_html=True
+)
+
+st.image("https://i.ibb.co/VWhVWh7s/IMG-20250720-WA0028.jpg", width=250, caption="For the best sister ever 💜")
+
+st.markdown("---")
+
+st.markdown("""
+### 
+
+Dear Harithra,
+I know you said a letter is enough, but I wanted to take a moment to express how much you mean to me.
+And you know that my handwriting is terrible, so I thought this would be a better way to share my feelings.
+Thank you for being more than just my sister you're my built-in best friend, therapist.
+I’ll protect you from weird people, bad vibes, and whatever life throws at you. 
+And if anyone messes with you, they’ll have me to deal with (and yes, that’s a serious threat).
+            
+You are the most amazing person I know, and I am so grateful to have you in my life. 
+Your strength, kindness, and sense of humor make every day brighter.
+I just miss the days we spend together.
+
+Love you,  
+**– \,
+Your brother Abinand(who’s definitely cooler than you, but don’t tell anyone) 😎**
+
+""")
+
+st.markdown("---")
+st.markdown("</h4>", unsafe_allow_html=True)
